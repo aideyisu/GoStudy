56eb5dffff6e2ec5ee944ce1e3a03f4f7af4bfd2
aideyisu <740531307@qq.com>
2021-01-16 20:43:53 +0800

    new:pkgerrors

--- ere/main.go
+++ ere/main.go
@@ -1,12 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/pkg/errors"
+)
 
 func main() {
 	fmt.Println("vim-go")
 	err := Z()
 	if err != nil {
 		fmt.Println(err)
+	}
+
+	Cause := errors.New("what")
+
+	e2 := errors.Wrapf(Cause, "hi")
+	if errors.Is(e2, Cause) {
+		fmt.Println("hello")
+	}
+
+	e3 := Dao(Cause)
+	fmt.Println(errors.Is(e3, Cause))
+	if errors.Is(e3, Cause) {
+		fmt.Println("hello")
 	}
 }
 
@@ -20,4 +36,8 @@ func X() error {
 
 func Z() error {
 	return X()
+}
+
+func Dao(Cause error) error {
+	return errors.Wrapf(Cause, "this is only a test.")
 }
